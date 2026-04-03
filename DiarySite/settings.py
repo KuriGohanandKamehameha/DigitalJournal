149034535911b90c3e7ce56417ea88c97a64ea36
Niveditha Mathew <nivedithamathew@gmail.com>
2026-04-04 02:38:54 +0530

    fixed structure v2

--- DiarySite/settings.py
+++ DiarySite/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'DiarySite1.urls'
+ROOT_URLCONF = 'DiarySite.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'DiarySite1.wsgi.application'
+WSGI_APPLICATION = 'DiarySite.wsgi.application'
 
 
 # Database
