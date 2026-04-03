7d6f6481b35d89da7aaa89970ec6726115c82056
Niveditha Mathew <nivedithamathew@gmail.com>
2026-04-04 02:48:33 +0530

    fixed structure v4

--- DiarySite/asgi.py
+++ DiarySite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for DiarySite1 project.
+ASGI config for DiarySite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- DiarySite/wsgi.py
+++ DiarySite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DiarySite1 project.
+WSGI config for DiarySite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
