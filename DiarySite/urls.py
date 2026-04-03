a143881ee17dd84acbab1ca339bbc59325b093de
Niveditha Mathew <nivedithamathew@gmail.com>
2026-04-04 03:29:24 +0530

    imported v2

--- DiarySite/urls.py
+++ DiarySite/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from entries import views
+import entries.views as views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
